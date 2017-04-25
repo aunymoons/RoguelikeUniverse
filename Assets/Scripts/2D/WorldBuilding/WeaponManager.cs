f7f78c208eeae9f38c444bb8b713899ff3023d70
Auny Moons <auny.moons@gmail.com>
2017-04-25 17:04:53 +0200

    Update WeaponManager.cs

--- Assets/Scripts/2D/WorldBuilding/WeaponManager.cs
+++ Assets/Scripts/2D/WorldBuilding/WeaponManager.cs
@@ -42,7 +42,7 @@ public class WeaponManager : Manager
 
         weaponArray[0, 2, 0] = new Weapon(4, "weapon1", new Vector3(0, 2, 0));
         weaponArray[0, 8, 0] = new Weapon(18, "weapon2", new Vector3(0, 8, 0));
-        weaponArray[0, 12, 0] = new Weapon(32, "weapon3", new Vector3(0, 12, 0));
+        weaponArray[0, 9, 0] = new Weapon(32, "weapon3", new Vector3(0, 12, 0));
         
     }
 
